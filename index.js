44ad2b30a11434808998f5abf9c060b8a68838db
Sourav <souravmalviya379@gmail.com>
2022-09-12 20:35:07 +0530

    added a controller and router for home

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//use express router
+app.use('/', require('./routes'))
+
 app.get('/', (req, res)=>{
     res.send("<h1>Welcome to Express Application</h1>")
 })
