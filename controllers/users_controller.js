1454e3feed1323a742ea968cde42470ed2d960df
Sourav <souravmalviya379@gmail.com>
2022-09-13 18:14:57 +0530

    added login and sign up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,29 @@
 module.exports.profile = function(req, res){
-    return res.render('profile', {
+    return res.render('user_profile', {
         title: "User Profile"
     })
+}
+
+module.exports.signUp = function(req, res){
+    return res.render('signUp', {
+        title: "Sign Up Page"
+    });
+}
+
+module.exports.login = function(req, res){
+    return res.render('login', {
+        title: 'Login Page'
+    });
+}
+
+// get the signUp data
+module.exports.create = function(req, res){
+    //To do later
+}
+
+//get the login data    
+module.exports.create_session = function(req, res){
+    //to do later
 }
 
 module.exports.posts = function(req, res){
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
+router.get('/signup', usersController.signUp);
+router.get('/login', usersController.login);
 router.get('/posts', usersController.posts);
 
 module.exports = router;
