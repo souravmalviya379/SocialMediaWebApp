e540afa392c91f137c5b4634947d48f207762589
Sourav <souravmalviya379@gmail.com>
2022-09-13 14:04:09 +0530

    added static paths and layouts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Home controller is set for SocialApp</h1>')
+    return res.render('home', {
+        title: 'Home'
+    })
 }
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+
+
 
 //use express router
 app.use('/', require('./routes'))
+
+//setting up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.get('/', (req, res)=>{
     res.send("<h1>Welcome to Express Application</h1>")
