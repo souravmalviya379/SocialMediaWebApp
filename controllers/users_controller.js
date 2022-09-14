3f19a2b704e2a78b3aba4de3e1ca3de83a0654ab
Sourav <souravmalviya379@gmail.com>
2022-09-14 17:00:07 +0530

    setup manual authentication using cookies\

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,21 @@
 const User = require("../models/user")
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: "User Profile"
-    })
+    if(req.cookies.user_id){
+        User.findOne({id: req.cookies.user_id}, function(err, user){
+            if(err){console.log('Error in finding user from cookies'); return;}
+
+            if(user){
+                return res.render('user_profile', {
+                    title: "User Profile", 
+                    user: user
+                })
+            }
+            return res.redirect('/users/login');
+        })
+    }else{
+        res.redirect('/users/login');
+    }
 }
 
 module.exports.signUp = function(req, res){
@@ -41,9 +53,34 @@ module.exports.create = function(req, res){
 
 //get the login data    
 module.exports.create_session = function(req, res){
-    //to do later
+    //find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding the user'); return;}
+
+        //handle the user found
+        if(user){
+            //check password match or not
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            //handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        }else{
+            return res.redirect('back');
+        }
+    });
 }
 
 module.exports.posts = function(req, res){
     return res.end('<h1>See the following posts</h1>');
+}
+
+
+module.exports.signOut = function(req, res){
+    if(req.cookies){
+        res.cookie('user_id', "");
+    }
+    return res.redirect('/users/login');
 }
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ router.get('/profile', usersController.profile);
 router.get('/signup', usersController.signUp);
 router.get('/login', usersController.login);
 router.get('/posts', usersController.posts);
+router.get('/sign-out', usersController.signOut);
 
 router.post('/create', usersController.create);
 router.post('/create_session', usersController.create_session);
