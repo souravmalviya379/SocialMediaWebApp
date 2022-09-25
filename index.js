f66097a55a9d3c17131ab7497175afef002ab03a
Sourav <souravmalviya379@gmail.com>
2022-09-25 16:17:47 +0530

    mongoStore used

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-const MongoStore = require('connect-mongo')(session);
+const MongoStore = require('connect-mongo');
+const sassMiddleware = require('node-sass-middleware');
 
+//Using sass before server starts as it should be ready before server serves it to the user
+app.use(sassMiddleware({
+    src: '/assets/scss',
+    dest: '/assets/css'
+}))
 app.use(express.urlencoded());
 app.use(cookieParser());
 
@@ -36,7 +42,7 @@ app.use(session({
     },
     store: new MongoStore(
         {
-            mongooseConnection: db,
+            mongoUrl: 'mongodb://localhost/socialApp_development',
             autoRemove: 'disabled'
         }, 
         function(err){
